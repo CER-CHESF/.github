6b32209aa1d92ccc31dd1d612d361c38b6579aeb
Marlos Ribeiro <550561+mgrb@users.noreply.github.com>
2026-01-28 18:37:37 -0300

    Remove satellite directories from data room setup
    Removed creation of satellite directories for goes16 and goes19.

--- profile/create_data_room.sh
+++ profile/create_data_room.sh
@@ -6,7 +6,6 @@ set -e
 mkdir -pv ./data/input/{obs,gfs,wrf_ctrl,wrf_solar,c3s,satellite,cmip6}
 mkdir -pv ./data/input/obs/{scada_eolico,tma,scada_solar,esol}
 mkdir -pv ./data/input/c3s/{ecmwf,eccc}
-mkdir -pv ./data/input/satellite/{goes16,goes19}
 
 mkdir -pv ./data/output/{p6_data_connector,p6_qa_global,p6_qa_local_eolico,p6_qa_local_solar,p6_sat_h,p6_ds_hd,p6_pcm_dir_hd,p6_pcm_ind_hd,p6_pcm_ind_fis_hd,p6_ts_hd,p6_comb_all}
 
