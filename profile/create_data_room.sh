f6f09d553bdef3e6dc863bd37cbef6975aaf94fb
Marlos Ribeiro <550561+mgrb@users.noreply.github.com>
2026-01-30 07:36:04 -0300

    Refactor directory creation in create_data_room.sh
    Removed unnecessary directories from output and params creation and added specific directories for p6_data_connector and p6_goes19_consumer.

--- profile/create_data_room.sh
+++ profile/create_data_room.sh
@@ -7,12 +7,9 @@ mkdir -pv ./data/input/{obs,gfs,wrf_ctrl,wrf_solar,c3s,satellite,cmip6}
 mkdir -pv ./data/input/obs/{scada_eolico,tma,scada_solar,esol}
 mkdir -pv ./data/input/c3s/{ecmwf,eccc}
 
-mkdir -pv ./data/output/{p6_data_connector,p6_qa_global,p6_qa_local_eolico,p6_qa_local_solar,p6_sat_h,p6_ds_hd,p6_pcm_dir_hd,p6_pcm_ind_hd,p6_pcm_ind_fis_hd,p6_ts_hd,p6_comb_all}
+mkdir -pv ./data/output/{p6_qa_global,p6_qa_local_eolico,p6_qa_local_solar,p6_sat_h,p6_ds_hd,p6_pcm_dir_hd,p6_pcm_ind_hd,p6_pcm_ind_fis_hd,p6_ts_hd,p6_comb_all}
 
-mkdir -pv ./data/params/{p6_data_connector,p6_qa_global,p6_qa_local_eolico,p6_qa_local_solar,p6_sat_h,p6_ds_hd,p6_pcm_dir_hd,p6_pcm_ind_hd,p6_pcm_ind_fis_hd,p6_ts_hd,p6_comb_all}
-
-mkdir -pv ./data/output/p6_goes19_consumer/logs
-
+mkdir -pv ./data/params/{p6_qa_global,p6_qa_local_eolico,p6_qa_local_solar,p6_sat_h,p6_ds_hd,p6_pcm_dir_hd,p6_pcm_ind_hd,p6_pcm_ind_fis_hd,p6_ts_hd,p6_comb_all}
 
 for d in ./data/output/*; do
     mkdir -pv "$d"/{train,operation,logs}
@@ -21,3 +18,8 @@ done
 for d in ./data/params/*; do
     mkdir -pv "$d"/{config,model_trained_state}
 done
+
+mkdir -pv ./data/params/p6_data_connector/config
+mkdir -pv ./data/output/p6_data_connector/logs
+
+mkdir -pv ./data/output/p6_goes19_consumer/logs
